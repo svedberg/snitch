d60bee1c08fba604e8ada041d0ae2bf15739b088
Glitch (snitch-me) <none>
2020-03-07 18:19:00 +0000

    👝🎠 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,81 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+/*var tunes = document.getElementById('playlist').querySelectorAll('li');
+var i;
+for (i = 0; i < tunes.length; i++) {
+  tunes[i].addEventListener("click", function(){
+    this.classList.toggle('active');
+  });
+}*/
+
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken('b65d6c4d1f5f451288cbedee9cbf93e6');
+
+
+function searchSpotify(code) {
+  spotifyApi.searchTracks('Love')
+  .then(function(data) {
+    console.log('Search by "Love"', data);
+  }, function(err) {
+    console.error(err);
+  });
+}
+
+function decode(src) {
+  console.log("lolo" + src);
+  Quagga.decodeSingle(
+    {
+      inputStream: {
+        size: 800,
+        singleChannel: false
+      },
+      locator: {
+        patchSize: "large",
+        halfSample: false
+      },
+      decoder: {
+        readers: [
+          {
+            format: "ean_reader",
+            config: {}
+          }
+        ]
+      },
+      locate: true, 
+      src: src 
+    },
+    function(result) {
+      if (result.codeResult) {
+        $("#coderes").text(result.codeResult.code);
+        searchSpotify(result.codeResult.code);
+      } else {
+        console.log("not detected");
+        $("#coderes").text("nope");
+      }
+    }
+  );
+}
+
+$(function() {
+
+  $("#btn input[type=file]").on("change", function(e) {
+    if (e.target.files && e.target.files.length) {
+        decode( URL.createObjectURL(e.target.files[0] ) );
+    }
+  });
+
+  $("#btn2 input[type=file]").on("change", function(e) {
+    if (e.target.files && e.target.files.length) {
+        decode( URL.createObjectURL(e.target.files[0] ) );
+    }
+  });
+
+  $(".wrapper-1 .reset").on("click", function(e) {
+    this.parent().removeClass('active');
+  });
+
+  $(".wrapper-2 .reset").on("click", function(e) {
+    alert('tewrdf');
+  });
+});
